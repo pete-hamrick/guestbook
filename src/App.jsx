8bf963781b998d369499b3c2138edb7b5def9f8d
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-13 20:40:36 -0800

    added home view that will eventually hold the form and guest entries, call it inside app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from './views/Home/Home';
 import Layout from './views/Layout/Layout';
 
 export default function App() {
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1>Hello World, I'm in the app component</h1>
+      <Home />
     </Layout>
   );
 }
