e08ffb2ad003ceafa39ad2724a9510ddc32c9edc
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-03 17:03:47 -0800

    wrote useForm hook

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 export default function Login() {
   const [error, setError] = useState(null);
